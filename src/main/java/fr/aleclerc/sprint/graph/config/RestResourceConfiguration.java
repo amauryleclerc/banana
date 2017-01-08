6ed36774e3bd376e80698c37e03387af7599430c
Pierre <ptrouillard@gmail.com>
2017-01-08 15:05:40 +0100

    Fix the deprecated warning for configure method, replaced it by RepositoryRestConfigurerAdapter method

--- src/main/java/fr/aleclerc/sprint/graph/config/RestResourceConfiguration.java
+++ src/main/java/fr/aleclerc/sprint/graph/config/RestResourceConfiguration.java
@@ -2,15 +2,16 @@ package fr.aleclerc.sprint.graph.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 import fr.aleclerc.sprint.graph.entities.Sprint;
 import fr.aleclerc.sprint.graph.entities.Story;
 @Configuration
-public class RestResourceConfiguration  extends RepositoryRestMvcConfiguration{
+public class RestResourceConfiguration  extends RepositoryRestConfigurerAdapter {
 
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Sprint.class,Story.class);
         config.setBasePath("/api");
     }
