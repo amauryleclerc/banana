722232198ee0ac86045b0cfc18b37ce3c0089bf6
soncoerok <guillaume.leloup.etu@gmail.com>
2017-07-03 22:56:24 +0200

    Correction backend + Avancement IHM "Releases"

--- frontend/src/main/frontend/src/app/components/releases/releases.component.ts
+++ frontend/src/main/frontend/src/app/components/releases/releases.component.ts
@@ -4,6 +4,7 @@ import { Release } from '../../models/release';
 import { NewReleaseComponent } from '../new-release/new-release.component';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-releases',
@@ -13,7 +14,9 @@ export class ReleasesComponent implements OnInit {
 
     public releases: Release[];
 
-    constructor(private releaseService: ReleaseService, private modalService: NgbModal) {
+    constructor(private releaseService: ReleaseService,//
+        private modalService: NgbModal,//
+        private router: Router) {
         this.releases = new Array<Release>();
     }
 
@@ -27,6 +30,18 @@ export class ReleasesComponent implements OnInit {
             this.modalService.open(NewReleaseComponent).result)//
             .flatMap(s => this.releaseService.save(s))//
             .subscribe(release => this.releases.unshift(release));
+    }
+
+    save(release: Release) {
+        // TODO
+    }
+
+    cancel(release: Release) {
+        // TODO
+    }
+
+    selectMore(release: Release) {
+        this.router.navigate(['/release', release.id]);
     }
 
 }
